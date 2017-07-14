8c4428f571369c91ae27ef8259f35ea3210777af
Renze Yu <renzeyu@microsoft.com>
2017-07-14 09:21:18 +0800

    add hechaochao to default reviewer

--- background.js
+++ background.js
@@ -23,6 +23,10 @@ const defaultReviewerInfos = [
         userName: "vwxyzh",
         userId: "2705828"
     },
+    {
+        userName: "hechaochao",
+        userId: "10389980"
+    },
 ]
 
 $(".discussion-sidebar").on('DOMSubtreeModified', addDefaultReviewerOfDocfx);
